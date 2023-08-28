8cf3cc1d4db70af20a5e4fc792897fd982524e04
Yalamachili Lokesh <119721652+YALAMANCHIL@users.noreply.github.com>
2023-08-28 20:49:34 +0530

    a=int(input()) b=int(input()) s=a+b p=a-b d=a*b n=a/b m=a%b print("Sum:%d"%(s)) print("Difference:%d"%(p)) print("Product:%d"%(d)) print("Quotient:%d"%(n)) print("Remainder:%d"%(m))

---
+++ Arithmetic_operations.py
@@ -0,0 +1,12 @@
+a=int(input())
+b=int(input())
+s=a+b
+p=a-b
+d=a*b
+n=a/b
+m=a%b
+print("Sum:%d"%(s))
+print("Difference:%d"%(p))
+print("Product:%d"%(d))
+print("Quotient:%d"%(n))
+print("Remainder:%d"%(m))
