4df5091967d768a835a8495c22b615c840511c7e
Yalamachili Lokesh <119721652+YALAMANCHIL@users.noreply.github.com>
2023-11-03 16:07:33 +0530

    m,n=map(int,input().split()) mat=[] for i in range(m):     inner_list=list(map(int,input().split()))[:n:]     mat.append(inner_list) s=0 s1=0 for inner_list in mat:     for every_value in inner_list:         if every_value%2==0:             s+=every_value         else:             s1+=every_value print(f"{s} {s1}")

---
+++ Sum_of_even_and__odd_elements_in_a_matrix.py
@@ -0,0 +1,14 @@
+m,n=map(int,input().split())
+mat=[]
+for i in range(m):
+    inner_list=list(map(int,input().split()))[:n:]
+    mat.append(inner_list)
+s=0
+s1=0
+for inner_list in mat:
+    for every_value in inner_list:
+        if every_value%2==0:        
+            s+=every_value
+        else:
+            s1+=every_value
+print(f"{s} {s1}")
